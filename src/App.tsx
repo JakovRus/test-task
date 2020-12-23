e4e6137cb182926b1d7439e83b9a5f0438b3a7a3
rogov <rogov@haulmont.com>
2020-12-23 14:29:27 +0400

    added task description

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+/*
+
+Есть два эндпоинта:
+
+https://my-json-server.typicode.com/paraplancrm/api/students (Список учеников)
+
+[
+  {
+    "name": "Надежда Калашникова",
+    "status": "38d4f887-add9-416a-8590-8352c6e28e13"
+  },
+]
+
+https://my-json-server.typicode.com/paraplancrm/api/statuses (Список статусов)
+
+[
+  {
+    "id": "38d4f887-add9-416a-8590-8352c6e28e13",
+    "name": "Записался на пробное",
+    "order": 20,
+    "active": true
+  },
+]
+
+
+1. Вывести список учеников в формате "Имя ученика - Название статуса".
+// 2. Разбить список на два - первый содержит только активных учеников, второй только неактивных.
+// 3. Отсортировать списки по полю order статуса и по фамилии ученика.
+
+fetch('url').then(res => res.json())
+
+*/
+
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
